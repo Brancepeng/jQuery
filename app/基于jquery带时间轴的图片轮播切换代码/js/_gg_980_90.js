8831a32b196fd21bdedad04d02dafccc6e8a154b
brancepeng <18761957987@163.com>
2015-06-09 17:14:26 +0800

    Initial commit

---
+++ app/基于jquery带时间轴的图片轮播切换代码/js/_gg_980_90.js
@@ -0,0 +1,6 @@
+
+//访问更多
+
+document.writeln('<div class="more" style="width:970px;margin:auto;margin-bottom:10px;">');
+document.writeln('<a target="_blank" href="http://www.w2bc.com/shili" style="display: block; width: 970px; margin: auto; height: 40px; line-height: 40px; text-align: center; background: #7FB41D;font-size: 18px; color: #FFF; text-decoration: none">点击这里查看更多实例 </a>');
+document.writeln('</div>');
